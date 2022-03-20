ff390d47553a76da08b3043928e02070616712f9
Maxssaf <101875980+Maxssaf@users.noreply.github.com>
2022-03-20 18:21:10 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -3,19 +3,8 @@
 sudo apt update -y
 sudo apt upgrade -y
 # Install git, python3
-sudo apt install git gcc libc-dev libffi-dev libssl-dev python3-dev rustc -y
-sudo apt install git -y
-sudo apt upgrade git -y
-sudo apt install wget -y
-sudo apt upgrade wget -y
-sudo apt install python3 -y
-sudo apt upgrade python3 -y
-sudo apt install python3-pip -y
-sudo apt upgrade python3-pip -y
-sudo apt install screen -y
-sudo apt upgrade screen -y
-sudo apt install curl -y
-sudo apt upgrade curl -y
+sudo apt install --upgrade git wget python3 python3-pip screen curl gcc libc-dev libffi-dev libssl-dev python3-dev rustc -y
+
 
 
 
@@ -25,7 +14,7 @@ sudo rm -r mhddos_proxy
 git clone https://github.com/porthole-ascend-cinnamon/mhddos_proxy.git
 cd mhddos_proxy
 sudo rm proxies_config.json
-sudo wget https://raw.githubusercontent.com/Aruiem234/mhddosproxy/main/proxies_config.json
+curl -o proxies_config.json https://raw.githubusercontent.com/Aruiem234/mhddosproxy/main/proxies_config.json
 sudo git clone https://github.com/MHProDev/MHDDoS.git
 sudo python3 -m pip install -r MHDDoS/requirements.txt
 cd ~
